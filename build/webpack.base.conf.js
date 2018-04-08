e1a92a3bf450a75e1e4bf163c1d121a0bbce40c3
Sangeethks <sangeethks.in@gmail.com>
2018-04-08 23:21:13 +0530

    webpack config updated

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,26 @@
+'use strict'
+const path = require('path')
+const config = require('../config')
+
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    path: config.prod.assetsRoot,
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: [ '.js', '.json' ]
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      include: [
+        resolve('src'),
+        resolve('test')
+      ]
+    }]
+  }
+}
--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,3 +2,17 @@
 const path = require('path')
 const webpackBaseConfig = require('./webpack.base.config.js')
 const config = require('../config')
+const webpack = require('webpack')
+const webpackMerge = require('webpack-merge')
+const copyWebpackPlugin = require('copy-webpack-plugin')
+
+const HOST = process.env.HOST
+const PORT = process.evn.PORT && Number(process.env.PORT)
+
+const webpackDevConfig = webpackMerge(webpackBaseConfig, {
+  devtool: config.dev.devtool,
+  devServer: {
+    clientLogLevel: 'warning',
+  },
+  watch: true
+})

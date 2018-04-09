54137b8a3006f99807802e0b21e0a0e8330b8f21
Sangeethks <sangeethks.in@gmail.com>
2018-04-09 20:27:04 +0530

    Success - dist compilation successfull

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,6 +1,29 @@
 'use strict'
-const path = require('path'),
-const config = require('config')
+const path = require('path')
+const config = require('../config')
 const webpackBaseConfig = require('./webpack.base.conf.js')
 const copyWebpackPlugin = require('copy-webpack-plugin')
 const uglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const merge = require('webpack-merge')
+
+const webpackProdConfig = merge(webpackBaseConfig, {
+  mode: 'production',
+  devtool: config.prod.devtool,
+  output: {
+    path: config.prod.assetsRoot,
+    filename: '[name].js'
+  },
+  plugins: [
+    new uglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          warnings: false
+        }
+      },
+      sourceMap: true,
+      parallel: true
+    })
+  ]
+})
+
+module.exports = webpackProdConfig
